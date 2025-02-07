87630f59599115a813e77fb6cf6f676651d55a41
Alejandro Amaya <tresmaster@protonmail.ch>
2025-02-07 15:44:49 -0500

    upd

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Ships from "./pages/Ships";
+import Pilots from "./pages/Pilots";
+import UpdateShip from "./pages/UpdateShip";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ships" element={<Ships />} />
+        <Route path="/pilots" element={<Pilots />} />
+        <Route path="/update-ship" element={<UpdateShip />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
